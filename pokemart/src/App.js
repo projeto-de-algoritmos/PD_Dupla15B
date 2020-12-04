178c8b708287b3138eddbe7d25611aebadeabef8
Caio <caiooliv97@gmail.com>
2020-12-04 03:38:39 -0300

    Criado estrutura inicial

---
+++ pokemart/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="cotainer">
+      <div class="header">
+        <p>alo</p>
+      </div>
+      <h1>Alo</h1>
+    </div>
+  );
+}
+
+export default App;
